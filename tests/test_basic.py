03655e12a8634b0a9c86ab24e21433c9d229f34b
JahnaviPolisetty <jahnavipolisetty3@example.com>
2026-06-13 20:14:00 +0530

    Added pytest test case

---
+++ tests/test_basic.py
@@ -0,0 +1,24 @@
+import sys
+import os
+
+sys.path.append(
+    os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "..", "backend")
+    )
+)
+
+from analyser import analyze_performance
+
+
+def test_analyze_performance():
+
+    data = {
+        "response_times": [100, 200, 300, 400, 500],
+        "requests": 1000,
+        "failed": 20
+    }
+
+    result = analyze_performance(data)
+
+    assert result["error_rate"] == 2.0
+    assert result["sla_status"] == "Healthy"
